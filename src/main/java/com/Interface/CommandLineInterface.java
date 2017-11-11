da2b7bfe55f341883d3213d32cda61c5f42f558f
Glenn Galvizo <glennjgalvizo@gmail.com>
2017-11-10 18:45:17 -1000

    CLI is "code-complete".
    Need to test... a lot.

--- src/main/java/com/Interface/CommandLineInterface.java
+++ src/main/java/com/Interface/CommandLineInterface.java
@@ -1,11 +1,11 @@
 package com.Interface;
 
 import com.ActivityNetwork.ActivityNetwork;
+import com.ActivityNetwork.ActivityNode;
 import com.ActivityNetwork.NetworkController;
 
 import java.io.Console;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 import static java.lang.System.exit;
 
@@ -25,6 +25,23 @@ class CommandLineInterface {
     }
 
     return false;
+  }
+
+  /**
+   * Ask the a question, and verify that their input is "Y" for yes, or "N" for no.
+   *
+   * @param c     Console object. Used to read responses from the user.
+   * @param query Question to ask the user.
+   * @return True if their answer to the query is yes. False otherwise.
+   */
+  private boolean yesOrNoQuestion(Console c, String query) {
+    String response = c.readLine(query + " [Y/N]");
+    while (!verifyResponse(response, new ArrayList<>(Arrays.asList("Y", "N")))) {
+      System.out.println("Invalid response. Please enter 'Y' for yes, or 'N' for no.");
+      response = c.readLine(query + " [Y/N]");
+    }
+
+    return query.equals("Y");
   }
 
   /**
@@ -104,7 +121,7 @@ class CommandLineInterface {
    * @return If the login is successful, a two element array of the authentication token, and the JSON array of project
    * IDs and names associated with the user. Otherwise, an empty array.
    */
-  public ArrayList<String> welcomeScreen(Console c) {
+  ArrayList<String> welcomeScreen(Console c) {
     boolean successfulLogin = false;
     ArrayList<String> tokenAndProject = new ArrayList<>();
 
@@ -112,27 +129,15 @@ class CommandLineInterface {
     while (!successfulLogin) {
 
       // First, we ask if they have an account with us.
-      String accountExistenceResponse = c.readLine("Do you have an account? [Y/N]");
-      while (!verifyResponse(accountExistenceResponse, new ArrayList<>(Arrays.asList("Y", "N")))) {
-        System.out.println("Invalid response. Please enter 'Y' for yes, or 'N' for no.");
-        accountExistenceResponse = c.readLine("Do you have an account? [Y/N]");
-      }
-
-      // If our answer is yes, then we bring them to our login screen.
-      if (accountExistenceResponse.equals("Y")) {
+      if (yesOrNoQuestion(c, "Do you have an account?")) {
+        // If our answer is yes, then we bring them to our login screen.
         tokenAndProject = loginConsole(c);
         successfulLogin = !tokenAndProject.isEmpty();
 
       } else {
         // If our answer is no, then we ask about account creation.
-        String accountCreationResponse = c.readLine("Would you like to create one? [Y/N]");
-        while (!verifyResponse(accountCreationResponse, new ArrayList<>(Arrays.asList("Y", "N")))) {
-          System.out.println("Invalid response. Please enter 'Y' for yes, or 'N' for no.");
-          accountCreationResponse = c.readLine("Would you like to create one? [Y/N]");
-        }
-
-        // If our answer is yes, then bring them to the account creation screen, then back to the login screen.
-        if (accountCreationResponse.equals("Y")) {
+        if (yesOrNoQuestion(c, "Would you like to create one?")) {
+          // If our answer is yes, then bring them to the account creation screen, then back to the login screen.
           accountCreationConsole(c);
 
         } else {
@@ -195,7 +200,7 @@ class CommandLineInterface {
    * @param projectJSON JSON array of Project IDs and names associated with a given user.
    * @return 0 if the user wants to exit. Otherwise, the network ID of the network the user wants to edit.
    */
-  public Long projectOverviewScreen(Console c, NetworkController nc, String projectJSON) {
+  Long projectOverviewScreen(Console c, NetworkController nc, String projectJSON) {
     boolean successfulSelection = false;
     long networkID = 0;
 
@@ -223,7 +228,7 @@ class CommandLineInterface {
           // Here, we verify that the project actually exists.
           ArrayList<String> names = UserAccount.namesFromProjectJSON(projectJSON);
           if (!names.contains(command[1])) {
-            System.out.println("Network does not exist. ");
+            System.out.println("Project does not exist. ");
 
           } else {
             successfulSelection = true;
@@ -233,8 +238,8 @@ class CommandLineInterface {
           break;
 
         case "delete":
-          System.out.println(!nc.deleteNetwork(networkID) ? "Network does not exist. " :
-              "Network successfully deleted. ");
+          System.out.println(!nc.deleteNetwork(networkID) ? "Project does not exist. " :
+              "Project successfully deleted. ");
           break;
       }
     }
@@ -247,19 +252,20 @@ class CommandLineInterface {
    *
    * @param c Console object. Used to read responses from the user.
    * @return A two element array of Strings, with the first entry representing the command, and the second
-   * representing the node name to change (if applicable).
+   * representing the argument to that command (if applicable).
    */
   private String[] acceptProjectSpecificCommand(Console c) {
     boolean isValidCommand = false;
 
     System.out.println("----------------------------------------------------");
     System.out.println("Enter 'exit' to return to the project overview screen.");
-    System.out.println("Enter 'add [node-name]' to add a node.");
-    System.out.println("Enter 'edit [node-name]' to edit an existing node.");
-    System.out.println("Enter 'remove [node-name]' to remove an existing node.");
-    System.out.println("Enter 'slack-total [node-name]' to view the total slack of a node.");
-    System.out.println("Enter 'slack-safety [node-name]' to view the safety slack of a node.");
-    System.out.println("Enter 'slack-free [node-name]' to view the free slack of a node.");
+    System.out.println("Enter 'add [activity-name]' to add a activity.");
+    System.out.println("Enter 'edit [activity-name]' to edit an existing activity.");
+    System.out.println("Enter 'remove [activity-name]' to remove an existing activity.");
+    System.out.println("Enter 'slack-total [activity-name]' to view the total slack of a activity.");
+    System.out.println("Enter 'slack-safety [activity-name]' to view the safety slack of a activity.");
+    System.out.println("Enter 'slack-free [activity-name]' to view the free slack of a activity.");
+    System.out.println("Enter 'set-deadline' to set the deadline of your entire project.");
     System.out.println("Enter 'view-network' to view the entire network.");
     System.out.println("Enter 'view-critical' to view the critical path.");
     System.out.println("Enter 'undo' to undo the most recent add/edit/remove.");
@@ -275,7 +281,8 @@ class CommandLineInterface {
         response = c.readLine("Please enter your option: ").split("\\s+");
 
       } else if (!verifyResponse(response[0], new ArrayList<>(Arrays.asList("exit", "add", "edit", "remove",
-          "slack-total", "slack-safety", "slack-free", "view-network", "view-critical", "undo", "redo")))) {
+          "slack-total", "slack-safety", "slack-free", "set-deadline", "view-network", "view-critical", "undo",
+          "redo")))) {
         System.out.println("Invalid response. Please only use the specified commands.");
         response = c.readLine("Please enter your option: ").split("\\s+");
 
@@ -288,15 +295,107 @@ class CommandLineInterface {
     return response;
   }
 
+
   /**
-   * Display the console to add a new node to the network.
+   * Get a valid time (double) in hours from the user.
+   *
+   * @param c            Console object. Used to read responses from the user.
+   * @param specificTime Pessimistic, normal, or optimistic time for the user to enter.
+   * @return A time that describes an activity, parsed from the user.
+   */
+  private double getValidTime(Console c, String specificTime) {
+    boolean isANumber = false;
+    double validTime = 0;
+
+    while (!isANumber) {
+      String timeGiven = c.readLine("Please enter the " + specificTime + " time (in hours): ");
+      try {
+        validTime = Double.valueOf(timeGiven);
+
+        if (validTime <= 0) {
+          System.out.println("Cannot have negative durations. Please enter a non-negative number. ");
+        } else {
+          isANumber = true;
+        }
+
+      } catch (NumberFormatException e) {
+        System.out.println("Not a number. Please enter a number. ");
+      }
+    }
+
+    return validTime;
+  }
+
+  /**
+   * Get valid dependencies for the given node.
+   *
+   * @param c        Console object. Used to read responses from the user.
+   * @param w        Working instance of our network.
+   * @param nodeName Name of node to add dependencies to.
+   * @return Set of valid dependencies for a given node.
+   */
+  private Set<Long> getValidDependencies(Console c, ActivityNetwork w, String nodeName) {
+    boolean desiredExit = false;
+    Set<Long> dependencies = new HashSet<>();
+
+    System.out.println("You are now setting your dependencies.");
+    if (yesOrNoQuestion(c, "Would you like to see all current activities in your project? ")) {
+      System.out.println(w.toString());
+    }
+
+    while (!desiredExit) {
+      String dependency = c.readLine("Enter a dependency, or hit enter to exit. ");
+      if (dependency.equals("")) {
+        desiredExit = true;
+
+      } else if (dependency.equals(nodeName)) {
+        System.out.println("An activity cannot depend on itself. ");
+
+      } else if (!w.isNodeInNetwork(nodeName)) {
+        System.out.println("Node does not exist. Please choose a node that exists.");
+
+      } else {
+        dependencies.add(w.nodeIdFromName(nodeName));
+
+      }
+    }
+
+    return dependencies;
+  }
+
+
+  /**
+   * Display the console to add a new node to the network. The node ID of this node is incremented from the current
+   * largest.
    *
    * @param c        Console object. Used to read responses from the user.
    * @param w        Working instance of our network.
    * @param nodeName Name of node to add.
+   * @return True if the node was added. False otherwise.
    */
-  private void addNodeConsole(Console c, ActivityNetwork w, String nodeName) {
-    // TODO: finish add node console.
+  private boolean addNodeConsole(Console c, ActivityNetwork w, String nodeName) {
+    if (w.isNodeInNetwork(nodeName)) {
+      System.out.println("Node exists in network. Please choose a unique name.");
+      return false;
+    }
+
+    // The new node ID is incremented from the node with the current largest node ID.
+    long nodeID = w.getNodeList().stream().max(
+        Comparator.comparingLong(ActivityNode::getNodeId)).orElse(null).getNodeId() + 1;
+
+    String description = c.readLine("Please describe your activity in one line: ");
+    double optimisticTime = getValidTime(c, "optimistic");
+    double normalTime = getValidTime(c, "normal");
+    double pessimisticTime = getValidTime(c, "pessimistic");
+    Set<Long> dependencies = getValidDependencies(c, w, nodeName);
+
+    if (yesOrNoQuestion(c, "Would you like to update your project deadline?")) {
+      w.setHoursDeadline(getValidTime(c, "deadline"));
+    }
+
+    ActivityNode n = new ActivityNode(nodeID, nodeName, description, optimisticTime, normalTime, pessimisticTime);
+    n.setDependencies(dependencies);
+    return w.insertNode(n);
   }
 
   /**
@@ -306,8 +405,55 @@ class CommandLineInterface {
    * @param w        Working instance of our network.
    * @param nodeName Name of node to edit.
    */
-  private void editNodeConsole(Console c, ActivityNetwork w, String nodeName) {
-    // TODO: finish edit node console.
+  private boolean editNodeConsole(Console c, ActivityNetwork w, String nodeName) {
+    if (!w.isNodeInNetwork(nodeName)) {
+      System.out.println("Node does not exist in network. Please choose an existing node.");
+      return false;
+    }
+    Long nodeID = w.nodeIdFromName(nodeName);
+    ActivityNode n = w.retrieveNode(nodeID);
+
+    // For each entry in our node, we ask if the user wants to update that field.
+    for (String entry : new ArrayList<>(Arrays.asList("name", "description", "optimistic time", "normal time",
+        "pessimistic time", "dependencies"))) {
+      boolean updateDesired = yesOrNoQuestion(c, "Would you like to update this node's " + entry + "?");
+
+      switch (entry) {
+        case "name":
+          String name = updateDesired ? c.readLine("Please enter a new node name: ") : n.getName();
+          while (w.isNodeInNetwork(nodeName) && updateDesired) {
+            System.out.println("There exists a node in the project with that name. Please choose another. ");
+            name = c.readLine("Please enter a new node name: ");
+          }
+          n.setName(name);
+          break;
+
+        case "description":
+          n.setDescription(updateDesired ? c.readLine("Please describe your activity in one line: ") :
+              n.getDescription());
+          break;
+
+        case "optimistic time":
+          n.setOptimisticTime(updateDesired ? getValidTime(c, "optimistic") : n.getTimes()[0]);
+          break;
+
+        case "normal time":
+          n.setNormalTime(updateDesired ? getValidTime(c, "normal") : n.getTimes()[1]);
+          break;
+
+        case "pessimistic time":
+          n.setPessimisticTime(updateDesired ? getValidTime(c, "pessimistic") : n.getTimes()[2]);
+          break;
+
+        case "dependencies":
+          n.setDependencies(getValidDependencies(c, w, nodeName));
+          break;
+      }
+    }
+
+    // Modification is node removal and insertion.
+    w.deleteNode(nodeID);
+    return w.insertNode(n);
   }
 
   /**
@@ -318,7 +464,7 @@ class CommandLineInterface {
    * @param nc        Network controller, created upon successful login.
    * @param networkID Network ID of the desired network to modify.
    */
-  public void projectSpecificScreen(Console c, NetworkController nc, Long networkID) {
+  void projectSpecificScreen(Console c, NetworkController nc, Long networkID) {
     boolean successfulSelection = false;
 
     while (!successfulSelection) {
@@ -347,6 +493,11 @@ class CommandLineInterface {
         case "slack-safety":
           System.out.println("The safety slack of \"" + command[1] + "\" is " +
               w.computeSafetySlack(w.nodeIdFromName(command[1])));
+          break;
+
+        case "set-deadline":
+          System.out.println((!w.setHoursDeadline(getValidTime(c, "deadline"))) ? "Deadline changed succesfully. " :
+              "Deadline not changed. Time is unrealistic. ");
           break;
 
         case "slack-free":
